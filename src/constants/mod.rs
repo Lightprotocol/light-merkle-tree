5e17f3781993623f674b26d81b7f2198facdda98
Michal Rostecki <vadorovsky@gmail.com>
2023-01-20 10:37:29 +0800

    Make the crate agnostic towards hash funtions
    Use traits from the `digest` crate to support various hash functions.

---
+++ src/constants/mod.rs
@@ -0,0 +1,3 @@
+pub mod poseidon;
+
+pub type ZeroBytes = [[u8; 32]; 19];
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,24 @@
-use light_poseidon::{
-    parameters::bn254_x5_3::poseidon_parameters, Poseidon, PoseidonBytesHasher, PoseidonError,
-    HASH_LEN,
-};
+use constants::ZeroBytes;
+use digest::{Digest, FixedOutputReset};
+use thiserror::Error;
 
 pub mod constants;
 
 pub const DATA_LEN: usize = 32;
+pub const HASH_LEN: usize = 32;
 pub const MAX_HEIGHT: usize = 9;
 pub const MERKLE_TREE_HISTORY_SIZE: usize = 256;
 
-pub trait Hasher {}
+#[derive(Error, Debug)]
+pub enum MerkleTreeError {
+    #[error("Could not convert slice to array")]
+    SliceToArray,
+}
 
-pub struct MerkleTree {
+pub struct MerkleTree<D>
+where
+    D: Digest + FixedOutputReset,
+{
     /// Height of the Merkle tree.
     pub height: usize,
     /// Subtree hashes.
@@ -24,25 +31,28 @@ pub struct MerkleTree {
     /// Current index of the root.
     pub current_root_index: usize,
 
-    /// Poseidon hasher.
-    poseidon_hasher: Box<dyn PoseidonBytesHasher>,
+    /// sha256 hasher.
+    hasher: D,
+    /// Initial bytes of the Merkle tree (with all leaves having zero value).
+    zero_bytes: ZeroBytes,
 }
 
-impl MerkleTree {
-    pub fn new(height: usize) -> Self {
+impl<D> MerkleTree<D>
+where
+    D: Digest + FixedOutputReset,
+{
+    pub fn new(height: usize, hasher: D, zero_bytes: ZeroBytes) -> Self {
         assert!(height > 0);
         assert!(height <= MAX_HEIGHT);
 
         let mut filled_subtrees = [[0; HASH_LEN]; MAX_HEIGHT];
 
         for i in 0..height {
-            filled_subtrees[i] = constants::ZERO_BYTES_MERKLE_TREE[i];
+            filled_subtrees[i] = zero_bytes[i];
         }
 
         let mut roots = [[0; HASH_LEN]; MERKLE_TREE_HISTORY_SIZE];
-        roots[0] = constants::ZERO_BYTES_MERKLE_TREE[height - 1];
-
-        let poseidon_hasher = Box::new(Poseidon::new(poseidon_parameters()));
+        roots[0] = zero_bytes[height - 1];
 
         MerkleTree {
             height,
@@ -50,23 +60,33 @@ impl MerkleTree {
             roots,
             next_index: 0,
             current_root_index: 0,
-            poseidon_hasher,
+            hasher,
+            zero_bytes,
         }
     }
 
-    fn hash(
+    pub fn hash(
         &mut self,
         leaf1: [u8; DATA_LEN],
         leaf2: [u8; DATA_LEN],
-    ) -> Result<[u8; HASH_LEN], PoseidonError> {
-        self.poseidon_hasher.hash_bytes(&[&leaf1, &leaf2])
+    ) -> Result<[u8; HASH_LEN], MerkleTreeError> {
+        // TODO(vadorovsky): The `digest` crate defines the `update` method both
+        // in `Digest` and `Update` trait separately, therefore we need to
+        // specify the trait explicitly here. That could be fixed by just
+        // removing the `update` method from the `Digest` trait.
+        Digest::update(&mut self.hasher, &leaf1);
+        Digest::update(&mut self.hasher, &leaf2);
+        Ok(
+            <[u8; HASH_LEN]>::try_from(self.hasher.finalize_reset().to_vec())
+                .map_err(|_| MerkleTreeError::SliceToArray)?,
+        )
     }
 
     pub fn insert(
         &mut self,
         leaf1: [u8; DATA_LEN],
         leaf2: [u8; DATA_LEN],
-    ) -> Result<(), PoseidonError> {
+    ) -> Result<(), MerkleTreeError> {
         // Check if next index doesn't exceed the Merkle tree capacity.
         assert_ne!(self.next_index, 2usize.pow(self.height as u32));
 
@@ -77,16 +97,16 @@ impl MerkleTree {
             "current level hash (hash of new leaves) {:?}",
             current_level_hash
         );
-        println!("starting the loop (0..height-1)");
+        println!("starting the loop (1..height)");
 
-        for i in 0..self.height - 1 {
+        for i in 1..self.height {
             // println!("current index: {current_index}");
             let (left, right) = if current_index % 2 == 0 {
-                // println!("assiging current hash to subtree {}", i);
+                println!("assiging current hash to subtree {}", i);
                 self.filled_subtrees[i] = current_level_hash;
 
                 // println!("current_hash = hash(current_hash, zeros[{i}])");
-                (current_level_hash, constants::ZERO_BYTES_MERKLE_TREE[i])
+                (current_level_hash, self.zero_bytes[i])
             } else {
                 // println!("current_hash = hash(filled_subtrees[{i}], current_hash)");
                 (self.filled_subtrees[i], current_level_hash)
