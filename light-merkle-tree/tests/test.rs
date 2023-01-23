dc5eb644d76b9f33229956431b8171233875ccab
Michal Rostecki <vadorovsky@gmail.com>
2023-01-23 20:27:57 +0800

    tests: More insertions

--- light-merkle-tree/tests/test.rs
+++ light-merkle-tree/tests/test.rs
@@ -18,7 +18,7 @@ fn test_merkle_tree_insert() {
     let zero_bytes = constants::sha256::ZERO_BYTES;
     let mut merkle_tree = MerkleTree::new(3, hasher, zero_bytes);
 
-    let h1 = merkle_tree.hash([1; 32], [1; 32]).unwrap();
+    let h1 = merkle_tree.hash([1; 32], [2; 32]).unwrap();
     println!("h1: {:?}", h1);
 
     let h2 = merkle_tree.hash(h1, zero_bytes[1]).unwrap();
@@ -28,7 +28,25 @@ fn test_merkle_tree_insert() {
     println!("h3: {:?}", h3);
 
     println!("root: {:?}", merkle_tree.last_root());
-    merkle_tree.insert([1u8; 32], [1u8; 32]).unwrap();
+    merkle_tree.insert([1u8; 32], [2u8; 32]).unwrap();
     println!("new root {:?}", merkle_tree.last_root());
-    assert_eq!(merkle_tree.last_root(), h3,);
+    assert_eq!(merkle_tree.last_root(), h3);
+
+    assert_eq!(
+        merkle_tree.last_root(),
+        [
+            247, 106, 203, 53, 197, 22, 54, 96, 235, 103, 77, 32, 26, 225, 24, 139, 161, 98, 253,
+            193, 16, 47, 34, 229, 111, 32, 89, 149, 147, 184, 120, 122
+        ]
+    );
+
+    merkle_tree.insert([3u8; 32], [4u8; 32]).unwrap();
+
+    assert_eq!(
+        merkle_tree.last_root(),
+        [
+            221, 141, 161, 139, 16, 93, 204, 253, 77, 161, 139, 239, 120, 252, 228, 149, 93, 68,
+            230, 151, 142, 173, 176, 130, 234, 217, 247, 60, 49, 232, 98, 116
+        ]
+    )
 }
