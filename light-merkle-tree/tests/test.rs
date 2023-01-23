dc31da788ae67a07165f2d4b8fd358850d5695b5
Michal Rostecki <vadorovsky@gmail.com>
2023-01-23 19:55:40 +0800

    tests: Add test for inserting to Merkle tree

--- light-merkle-tree/tests/test.rs
+++ light-merkle-tree/tests/test.rs
@@ -12,44 +12,23 @@ fn test_sha256() {
     assert_eq!(h, constants::sha256::ZERO_BYTES[0]);
 }
 
-// #[test]
-// fn test_merkle_tree_insert() {
-//     let mut merkle_tree = MerkleTree::new(3);
-//
-//     let h1 = merkle_tree.hash([1; 32], [1; 32]).unwrap();
-//     println!("h1: {:?}", h1);
-//
-//     let h2 = merkle_tree
-//         .hash(h1, constants::ZERO_BYTES_MERKLE_TREE[1])
-//         .unwrap();
-//     println!("h2: {:?}", h2);
-//
-//     let h3 = merkle_tree
-//         .hash(h2, constants::ZERO_BYTES_MERKLE_TREE[2])
-//         .unwrap();
-//     println!("h3: {:?}", h3);
-//
-//     println!("root: {:?}", merkle_tree.last_root());
-//     merkle_tree.insert([1u8; 32], [1u8; 32]).unwrap();
-//     println!("new root {:?}", merkle_tree.last_root());
-//     assert_eq!(
-//         merkle_tree.last_root(),
-//         [
-//             193, 191, 68, 0, 70, 193, 23, 91, 118, 42, 46, 219, 135, 229, 57, 186, 170, 251, 201,
-//             228, 159, 107, 47, 44, 109, 206, 191, 9, 202, 185, 30, 19
-//         ]
-//     );
-// }
+#[test]
+fn test_merkle_tree_insert() {
+    let hasher = Sha256::new();
+    let zero_bytes = constants::sha256::ZERO_BYTES;
+    let mut merkle_tree = MerkleTree::new(3, hasher, zero_bytes);
 
-// #[test]
-// fn test_full_merkle_tree_insert() {
-//     let mut merkle_tree = FullMerkleTree::new(3);
-//     merkle_tree.insert([3u8; 32], [3u8; 32]).unwrap();
-//     assert_eq!(
-//         merkle_tree.root(),
-//         [
-//             193, 191, 68, 0, 70, 193, 23, 91, 118, 42, 46, 219, 135, 229, 57, 186, 170, 251, 201,
-//             228, 159, 107, 47, 44, 109, 206, 191, 9, 202, 185, 30, 19
-//         ]
-//     );
-// }
+    let h1 = merkle_tree.hash([1; 32], [1; 32]).unwrap();
+    println!("h1: {:?}", h1);
+
+    let h2 = merkle_tree.hash(h1, zero_bytes[1]).unwrap();
+    println!("h2: {:?}", h2);
+
+    let h3 = merkle_tree.hash(h2, zero_bytes[2]).unwrap();
+    println!("h3: {:?}", h3);
+
+    println!("root: {:?}", merkle_tree.last_root());
+    merkle_tree.insert([1u8; 32], [1u8; 32]).unwrap();
+    println!("new root {:?}", merkle_tree.last_root());
+    assert_eq!(merkle_tree.last_root(), h3,);
+}
